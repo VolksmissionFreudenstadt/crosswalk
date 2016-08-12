632d2ce94b63721717dbe0c4d1a5081f6621d31a
Christoph Fischer <chris@toph.de>
2016-08-12 12:11:51 +0200

    Konstante "false" durch 0 ersetzt

--- .travis/schuelerheft.py
+++ .travis/schuelerheft.py
@@ -9,7 +9,7 @@ import os
 
 if scribus.haveDoc() :
     filename = os.path.splitext(scribus.getDocName())[0]
-    scribus.setLayerPrintable("Loesungen", false)
+    scribus.setLayerPrintable("Loesungen", 0)
     pdf = scribus.PDFfile()
     pdf.file = filename+".pdf"
     pdf.save()
